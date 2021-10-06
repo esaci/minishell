0e21e90124cd32b5c8b39c06c8ee416243d42181
Elias SACI <esaci@e1r12p9.42.fr>
2021-10-06 20:10:11 +0200

    auto

--- srcs/exec_files/exec_pipe.c
+++ srcs/exec_files/exec_pipe.c
@@ -21,10 +21,10 @@ t_node	*exec_pipe(t_lexer *l, t_node *n)
 	l->pip->pid[(count * 2)] = fork();
 	if (!l->pip->pid[(count * 2)])
 	{
-		/* if (count > 0)
+		if (count > 0)
 			dup2(l->pip->ppd[count - 1], STDIN_FILENO);
 		if (n->right && n->right->type == NODE_PIPE)
-			dup2(l->pip->ppd[count], STDOUT_FILENO); */
+			dup2(l->pip->ppd[count], STDOUT_FILENO);
 		exit_code = exec_com(l, n->left, id_pipe(l, n));
 		full_close(l);
 		full_free(l);
