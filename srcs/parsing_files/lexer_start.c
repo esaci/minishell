c7b2753e7bad128e111563f1d75ccf0185952502
Elias SACI <esaci@e3r12p16.42.fr>
2021-10-10 00:39:25 +0200

    stable

--- srcs/parsing_files/lexer_start.c
+++ srcs/parsing_files/lexer_start.c
@@ -61,7 +61,7 @@ int	init_lexer(t_lexer *lexer, char **envp)
 	return (0);
 }
 
-t_token	*lexer_token(char *str, char *oldstr)
+t_token	*lexer_token(char *str, char *oldstr, int *mode)
 {
 	t_token		*toktmp;
 	int			count;
@@ -70,8 +70,12 @@ t_token	*lexer_token(char *str, char *oldstr)
 	if (!(toktmp))
 		return (NULL);
 	count = 0;
-	while (str && str[count] == ' ')
-		count++;
+	if (!(*mode))
+	{
+		while (str && str[(*mode)] == ' ')
+			(*mode)++;
+		(*mode)++;
+	}
 	(toktmp)->line = str + count;
 	(toktmp)->type = lexer_check_type(toktmp, oldstr);
 	(toktmp)->n_token = NULL;
@@ -80,17 +84,17 @@ t_token	*lexer_token(char *str, char *oldstr)
 
 int	lexer_start(t_lexer *lexer)
 {
-	int			count;
 	t_token		**toktmp;
+	int			mode;
 
 	if (!rl_line_buffer)
 		return (1);
-	lexer->tok = lexer_token(rl_line_buffer, rl_line_buffer);
-	count = 1;
+	mode = 0;
+	lexer->tok = lexer_token(rl_line_buffer, rl_line_buffer, &mode);
 	toktmp = &(lexer->tok->n_token);
-	while(rl_line_buffer[count])
+	while(rl_line_buffer[mode])
 	{
-		(*toktmp) = lexer_token(rl_line_buffer + count, rl_line_buffer + count - 1);
+		(*toktmp) = lexer_token(rl_line_buffer + mode, rl_line_buffer + mode - 1, &mode);
 		lexer->len++;
 		if (!(*toktmp))
 		{
@@ -98,7 +102,7 @@ int	lexer_start(t_lexer *lexer)
 			return (1);
 		}
 		toktmp = &((*toktmp)->n_token);
-		count++;
+		mode++;
 	}
 	return (0);
 }
