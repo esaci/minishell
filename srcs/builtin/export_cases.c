e273f2291772c10c264c1c6c700cfee9f304b7f9
Jules PELLETIER <julpelle@e3r10p20.clusters.42paris.fr>
2021-11-02 01:57:37 +0100

    export fixed (stable)

--- srcs/builtin/export_cases.c
+++ srcs/builtin/export_cases.c
@@ -6,7 +6,7 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 12:00:06 by julpelle          #+#    #+#             */
-/*   Updated: 2021/10/30 12:16:39 by julpelle         ###   ########.fr       */
+/*   Updated: 2021/11/02 01:55:58 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,20 +41,16 @@ void	export_exist_equal(t_list *e, char *var, char *ptr)
 	char	*ptr2;
 	char	*ptr3;
 
-	if (ptr && ptr[0])
-	{
-		ptr2 = ft_strjoin(var, "=");
-		ptr3 = ft_strjoin(var, "\"");
-		free(ptr2);
-		ptr2 = ft_strjoin(ptr3, ptr);
-		free(ptr3);
-		ptr3 = ft_strjoin(ptr2, "\"");
-		ft_add_env(ptr3, e);
-		free(ptr2);
-		free(ptr3);
-	}
-	else
-		ft_add_env(var, e);
+
+	ptr2 = ft_strjoin(var, "=");
+	ptr3 = ft_strjoin(ptr2, "\"");
+	free(ptr2);
+	ptr2 = ft_strjoin(ptr3, ptr);
+	free(ptr3);
+	ptr3 = ft_strjoin(ptr2, "\"");
+	ft_add_env(ptr3, e);
+	free(ptr2);
+	free(ptr3);
 }
 
 void	export_with_plus(char *c_arg, t_list *e, char *var, char *o_v)
@@ -119,7 +115,7 @@ void	export_cases(char *arg, t_list *e)
 	var = get_var(arg);
 	ptr = custom_getenv(e, var, 0);
 	ft_del_variable(var, e);
-	if (check_all_char(get_value(arg)) == 1 && !ft_memcmp(ptr, "", 1))
+	if (check_all_char(get_value(arg)) == 1 && ft_memcmp(ptr, "", 1))
 		flag = 1;
 	if (flag == 1)
 		export_exist_equal(e, var, ptr);
@@ -129,6 +125,8 @@ void	export_cases(char *arg, t_list *e)
 		export_exist_empty(arg, e, var);
 	else if (check_variable(arg) == 3 && flag == 0)
 		ft_add_env(arg, e);
+	printf("arg : %s\n", arg);
+	printf("flag : %d\n", flag);
 	free(var);
 	free(ptr);
 }
