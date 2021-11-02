4aae1a30ef7a772bc99d992bfc24565d3c4de73d
Jules PELLETIER <julpelle@e3r10p20.clusters.42paris.fr>
2021-11-02 03:35:25 +0100

    Exec com norm In progress:

--- srcs/exec_files/exec_com.c
+++ srcs/exec_files/exec_com.c
@@ -6,7 +6,7 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 20:17:45 by esaci             #+#    #+#             */
-/*   Updated: 2021/11/02 01:01:08 by julpelle         ###   ########.fr       */
+/*   Updated: 2021/11/02 03:16:10 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,9 @@ int	last_pipe(t_lexer *l)
 	return (count);
 }
 
-int	exec_com(t_lexer *l, t_node *n, int count)
+void	exec_com_pipes(t_lexer *l, t_node *n, int count)
 {
-	int		tmp;
-	int		fd[2];
-	t_token	*t;
-	char	**ptr;
-	DIR		*dir_ptr;
+	int	tmp;
 
 	tmp = -1;
 	if (last_pipe(l) == 0 || count == last_pipe(l))
@@ -58,46 +54,109 @@ int	exec_com(t_lexer *l, t_node *n, int count)
 	}
 	else
 		l->pip->pid[count] = 0;
+}
+
+void	free_last_pipe(t_lexer *l, int count)
+{
+	if (count == last_pipe(l))
+	{
+		close_pipes(l, 1);
+		small_free(l, NULL, NULL, 1);
+		exit(print_custom("Empty pipe", 2, 1, 1));
+	}
+	close_pipes(l, 1);
+	small_free(l, NULL, NULL, 1);
+	exit(1);
+}
+
+void	exec_com_end(t_lexer *l, t_node *n, char **ptr, int tmp)
+{
+	DIR		*dir_ptr;
+	
+	if (execve(n->str[0], n->str, ptr) == -1)
+	{
+		dir_ptr = opendir(n->str[0]);
+		if (dir_ptr)
+		{
+			closedir(dir_ptr);
+			print_custom(n->str[0], 2, 1, 0);
+			tmp = 1;
+		}
+		else
+			tmp = 0;
+		double_free(ptr);
+		small_free(l, NULL, NULL, 1);
+		if (!tmp)
+			exit(print_custom("error comm", 2, 1, 1));
+		exit(print_custom(": Is a Directory", 2, 126, 1));
+	}
+}
+
+void	free_lexer_tmp(t_lexer *l, t_node *n, int tmp)
+{
+	if (tmp || !n->str[0])
+	{
+		if (n->str[0])
+		{
+			perror(n->str[0]);
+			if (!access(n->str[0], F_OK))
+			{
+				small_free(l, NULL, NULL, 1);
+				exit(126);
+			}
+			small_free(l, NULL, NULL, 1);
+			exit(127);
+		}
+		small_free(l, NULL, NULL, 1);
+		exit(0);
+	}
+}
+
+void	init_ptr(t_lexer *l, t_node *n, int fd[2])
+{
+	char	**ptr;
+
+	ptr = malloc(sizeof(char *) * 3);
+	if (!ptr)
+		exit(small_free(l, NULL, NULL, 1));
+	ptr[2] = NULL;
+	ptr[0] = open_infiles(n->left, &(fd[0]));
+	ptr[1] = open_outfiles(n->right, &(fd[1]));
+	if (fd[0] < 0 || fd[1] < 0)
+		exit(check_order_redirection(l, ptr));
+	free(ptr[0]);
+	free(ptr[1]);
+	free(ptr);
+	if (fd[1] != 1)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+	}
+	if (fd[0] != 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+	}
+}
+
+int	exec_com(t_lexer *l, t_node *n, int count)
+{
+	int		fd[2];
+	t_token	*t;
+	char	**ptr;
+	int		tmp;
+
+	exec_com_pipes(l, n, count);
 	if (!l->pip->pid[count])
 	{
 		if (!n || (n->type != NODE_NOCOM && n->type != NODE_PATHCOM))
-		{
-			if (count == last_pipe(l))
-			{
-				close_pipes(l, 1);
-				small_free(l, NULL, NULL, 1);
-				exit(print_custom("Empty pipe", 2, 1, 1));
-			}
-			close_pipes(l, 1);
-			small_free(l, NULL, NULL, 1);
-			exit(1);
-		}
+			free_last_pipe(l, count);
 		if (count != 0 && count == last_pipe(l))
 		{
 			close(l->pip->ppd[((count - 1) * 2) + 1]);
 			dup2(l->pip->ppd[(count - 1) * 2], STDIN_FILENO);
 		}
-		ptr = malloc(sizeof(char *) * 3);
-		if (!ptr)
-			exit(small_free(l, NULL, NULL, 1));
-		ptr[2] = NULL;
-		ptr[0] = open_infiles(n->left, &(fd[0]));
-		ptr[1] = open_outfiles(n->right, &(fd[1]));
-		if (fd[0] < 0 || fd[1] < 0)
-			exit(check_order_redirection(l, ptr));
-		free(ptr[0]);
-		free(ptr[1]);
-		free(ptr);
-		if (fd[1] != 1)
-		{
-			dup2(fd[1], STDOUT_FILENO);
-			close(fd[1]);
-		}
-		if (fd[0] != 0)
-		{
-			dup2(fd[0], STDIN_FILENO);
-			close(fd[0]);
-		}
+		init_ptr(l, n, fd);
 		ultime_close_archive(l);
 		close_pipes(l, 1);
 		if (menu(n->str[0], n->str + 1, l) == -1)
@@ -115,40 +174,9 @@ int	exec_com(t_lexer *l, t_node *n, int count)
 			n->str[0] = l->buffer[get_buffer_count(l, t)];
 			tmp = access(n->str[0], X_OK);
 		}
-		if (tmp || !n->str[0])
-		{
-			if (n->str[0])
-			{
-				perror(n->str[0]);
-				if (!access(n->str[0], F_OK))
-				{
-					small_free(l, NULL, NULL, 1);
-					exit(126);
-				}
-				small_free(l, NULL, NULL, 1);
-				exit(127);
-			}
-			small_free(l, NULL, NULL, 1);
-			exit(0);
-		}
+		free_lexer_tmp(l, n, tmp);
 		ptr = generate_custom_envp(l->envp);
-		if (execve(n->str[0], n->str, ptr) == -1)
-		{
-			dir_ptr = opendir(n->str[0]);
-			if (dir_ptr)
-			{
-				closedir(dir_ptr);
-				print_custom(n->str[0], 2, 1, 0);
-				tmp = 1;
-			}
-			else
-				tmp = 0;
-			double_free(ptr);
-			small_free(l, NULL, NULL, 1);
-			if (!tmp)
-				exit(print_custom("error comm", 2, 1, 1));
-			exit(print_custom(": Is a Directory", 2, 126, 1));
-		}
+		exec_com_end(l, n, ptr, tmp);
 	}
 	return (0);
 }
--- srcs/exec_files/exec_heredoc.c
+++ srcs/exec_files/exec_heredoc.c
@@ -6,59 +6,11 @@
 /*   By: julpelle <julpelle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 23:17:53 by esaci             #+#    #+#             */
-/*   Updated: 2021/11/02 02:14:44 by julpelle         ###   ########.fr       */
+/*   Updated: 2021/11/02 02:28:04 by julpelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../lib/libmin.h"
-
-int	countain_doll(char *ptr)
-{
-	int	count;
-
-	count = 0;
-	while (ptr && ptr[count])
-	{
-		if (ptr[count] == '$')
-			return (1);
-		count++;
-	}
-	return (0);
-}
-
-int	len_var(t_lexer *l, char *ptr)
-{
-	int		count;
-	int		count2;
-	char	*str;
-	char	*var;
-
-	count = 0;
-	count2 = 0;
-	while (ptr && ptr[count])
-	{
-		if (ptr[count] == '$')
-		{
-			var = get_name(ptr + count + 1);
-			str = custom_getenv(l->envp, var, 0);
-			count2 += ft_strlen(str);
-			free(var);
-			free(str);
-		}
-		count++;
-	}
-	return (count2);
-}
-
-void	copy_arg(char *str, char *ptr, int *count2)
-{
-	int	count3;
-
-	count3 = 0;
-	while (ptr && ptr[count3])
-		str[(*count2)++] = ptr[count3++];
-	free(ptr);
-}
 
 char	*apply_doll_suite(char *ptr, t_lexer *l, int count2, char *str)
 {
